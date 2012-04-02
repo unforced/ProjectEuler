7f07277e9ac22c8ab2be0c5c196bc4e1858fb3dd
Aaron Neyer <aaronneyer@gmail.com>
2012-04-02 20:28:56 +0000

    Shorter 19

--- euler19.py
+++ euler19.py
@@ -1,7 +1,3 @@
 import calendar
-x=0
 c=calendar.Calendar()
-for year in range(1901,2001):
-	for month in range(1,13):
-		x+=[0,0,0,0,0,0,1] in c.monthdayscalendar(year,month)
-print x
+print len([1 for month in range(1,13) for year in range(1901,2001) if [0,0,0,0,0,0,1] in c.monthdayscalendar(year,month)])
